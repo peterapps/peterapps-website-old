ee7474e1f9f95182786b3ab9f918f4467498204f
peterapps <peterapps2016@gmail.com>
2016-03-24 18:35:26 -0400

    Fixed some javascript

--- js/my-app.js
+++ js/my-app.js
@@ -10,8 +10,3 @@ var view1 = myApp.addView('#view-1', {
     // Because we use fixed-through navbar we can enable dynamic navbar
     dynamicNavbar: true
 });
-
-var mySearchbar = myApp.searchbar('.searchbar', {
-    searchList: '.list-block-search',
-    searchIn: '.item-title'
-});
