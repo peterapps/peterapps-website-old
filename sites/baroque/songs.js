640369cb41ec803dfc31126771d3c251f3bdef30
peterapps <peterapps2016@gmail.com>
2017-03-23 18:14:07 -0400

    New page for Baroque songs

---
+++ sites/baroque/songs.js
@@ -0,0 +1,159 @@
+var songs = [
+	{
+		"file": "Winter - Mvt I.mp3",
+		"composer": "Vivaldi",
+		"name": "Four Seasons: Winter",
+		"movement": "I",
+		"duration": 200
+	},
+	{
+		"file": "Winter - Mvt II.mp3",
+		"composer": "Vivaldi",
+		"name": "Four Seasons: Winter",
+		"movement": "II",
+		"duration": 119
+	},
+	{
+		"file": "Spring - Mvt I.mp3",
+		"composer": "Vivaldi",
+		"name": "Four Seasons: Spring",
+		"movement": "I",
+		"duration": 113
+	},
+	{
+		"file": "Summer - Mvt III.mp3",
+		"composer": "Vivaldi",
+		"name": "Four Seasons: Summer",
+		"movement": "III",
+		"duration": 159
+	},
+	{
+		"file": "Autumn - Mvt I.mp3",
+		"composer": "Vivaldi",
+		"name": "Four Seasons: Autumn",
+		"movement": "I",
+		"duration": 279
+	},
+	{
+		"file": "Concerto Grosso.mp3",
+		"composer": "Corelli",
+		"name": "Concerto Grosso Op. 6 No. 8",
+		"duration": 66
+	},
+	{
+		"file": "No. 2 - Mvt III.mp3",
+		"composer": "Bach",
+		"name": "Brandenburg Concerto No. 2",
+		"movement": "III",
+		"duration": 161
+	},
+	{
+		"file": "No. 3 - Mvt I.mp3",
+		"composer": "Bach",
+		"name": "Brandenburg Concerto No. 3",
+		"movement": "I",
+		"duration": 334
+	},
+	{
+		"file": "No. 4 - Mvt I.mp3",
+		"composer": "Bach",
+		"name": "Brandenburg Concerto No. 4",
+		"movement": "I",
+		"duration": 421
+	},
+	{
+		"file": "Toccata and Fugue in D minor.mp3",
+		"composer": "Bach",
+		"name": "Toccata and Fugue in D minor",
+		"duration": 512
+	},
+	{
+		"file": "Little Fugue in G minor.mp3",
+		"composer": "Bach",
+		"name": "Little Fugue in G minor",
+		"duration": 220
+	},
+	{
+		"file": "Overture.mp3",
+		"composer": "Handel",
+		"name": "Water Music",
+		"movement": "Overture",
+		"duration": 135
+	},
+	{
+		"file": "Hornpipe.mp3",
+		"composer": "Handel",
+		"name": "Water Music",
+		"movement": "Hornpipe",
+		"duration": 200
+	},
+	{
+		"file": "Lentement.mp3",
+		"composer": "Handel",
+		"name": "Water Music",
+		"movement": "Lentement",
+		"duration": 87
+	},
+	{
+		"file": "Bouree.mp3",
+		"composer": "Handel",
+		"name": "Water Music",
+		"movement": "Bouree",
+		"duration": 55
+	},
+	{
+		"file": "Minuet.mp3",
+		"composer": "Handel",
+		"name": "Water Music",
+		"movement": "Minuet",
+		"duration": 138
+	},
+	{
+		"file": "Overture 2_00.mp3",
+		"composer": "Handel",
+		"name": "Royal Fireworks",
+		"movement": "Overture",
+		"duration": 120
+	},
+	{
+		"file": "Bourree.mp3",
+		"composer": "Handel",
+		"name": "Royal Fireworks",
+		"movement": "Bourrée",
+		"duration": 86
+	},
+	{
+		"file": "La Paix.mp3",
+		"composer": "Handel",
+		"name": "Royal Fireworks",
+		"movement": "La Paix",
+		"duration": 223
+	},
+	{
+		"file": "La Rejouissance.mp3",
+		"composer": "Handel",
+		"name": "Royal Fireworks",
+		"movement": "The Rejoice",
+		"duration": 217
+	},
+	{
+		"file": "Minuets I and II.mp3",
+		"composer": "Handel",
+		"name": "Royal Fireworks",
+		"movement": "Minuet",
+		"duration": 278
+	},
+	{
+		"file": "Cello Suite No. 1 - Movement I.mp3",
+		"composer": "Bach",
+		"name": "Cello Suite No. 1",
+		"movement": "I",
+		"duration": 135
+	},
+	{
+		"file": "The Goldberg Variations.mp3",
+		"composer": "Bach",
+		"name": "The Goldberg Variations",
+		"duration": 300
+	},
+];
