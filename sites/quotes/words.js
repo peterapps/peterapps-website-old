05c1b4fa4042d301da9252109449fcd3c5815c05
peterapps <peterapps2016@gmail.com>
2017-11-15 17:03:26 -0500

    New quotes

---
+++ sites/quotes/words.js
@@ -0,0 +1,58 @@
+var words = {
+	"audience": [
+		"sonny,",
+		"dear boy,",
+		"idiot,"
+	],
+	"objects": [
+		"sadness",
+		"invention",
+		"realization",
+		"journey",
+		"action",
+		"misery",
+		"rebirth",
+		"genius",
+		"insanity",
+		"beauty",
+		"death",
+		"the world",
+		"inner beauty",
+		"true character",
+		"virtue",
+		"perfection",
+		"failure",
+		"love",
+		"friendship",
+		"life"
+	],
+	"adverbs": [
+		"truely",
+		"always",
+		"never",
+		"typically",
+		"rarely"
+	],
+	"verbs": [
+		"makes",
+		"transforms",
+		"reveals",
+		"creates",
+		"destroys",
+		"reflects",
+		"is",
+		"inspires",
+		"beholds",
+		"causes",
+		"touches",
+		"encourages",
+		"helps"
+	],
+	"prepositionals": [
+		"for yourself",
+		"for everyone",
+		"deep down",
+		"from inside out"
+	]
+};
+words.subjects = words.objects;
