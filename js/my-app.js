776464a11b9c845b8adc10986b15feaf89110103
peterapps <peterapps2016@gmail.com>
2017-11-21 18:43:14 -0500

    Add promo to subpages

--- js/my-app.js
+++ js/my-app.js
@@ -11,3 +11,7 @@ var view1 = myApp.addView('#view-1', {
     // Because we use fixed-through navbar we can enable dynamic navbar
     dynamicNavbar: true
 });
+
+if (location.hash == "#home") myApp.showTab("#view-1");
+if (location.hash == "#faq") myApp.showTab("#view-2");
+if (location.hash == "#more") myApp.showTab("#view-3");
